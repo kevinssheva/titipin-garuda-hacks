89679a6e0ed237ebffc7e775bac44380554279b5
kevinssheva <kevinssheva@gmail.com>
2023-07-21 19:14:04 +0700

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mariner: {
+          50: "#f0f8fe",
+          100: "#ddeefc",
+          200: "#c3e3fa",
+          300: "#9ad2f6",
+          400: "#6ab9f0",
+          500: "#479cea",
+          600: "#3280de",
+          700: "#2867c5",
+          800: "#2856a5",
+          900: "#254a83",
+          950: "#1b2e50",
+        },
+      },
+    },
   },
   plugins: [],
 };
