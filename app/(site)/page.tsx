4abcfe33206861b968096a2ca3460c80b1b812a3
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-07-23 04:48:51 +0700

    Routing, landing, also styling

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,13 +3,38 @@ import Landing from "@/app/(site)/components/landing/Landing";
 import Navbar from "../components/Navbar/Navbar";
 import RegisterModal from '../components/Modals/RegisterModal'
 import LoginModal from '../components/Modals/LoginModal'
+import { prisma } from "@/app/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import { Session } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions) as Session;
+
+  const recommendation = async () => {
+    return await prisma.post.findMany({
+      where: {
+        category: {
+          in: session?.user.categoryPilihan,
+        }
+      },
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        location: true,
+        stock: true,
+        imageURLs: true,
+      }
+    });
+  }
+
+  const recommendationProducts = await recommendation();
   return (
     <>
       <RegisterModal />
       <LoginModal />
-      <Landing />
+      <Landing recommendationProducts={recommendationProducts} />
     </>
   );
 }
