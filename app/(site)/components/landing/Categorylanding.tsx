02f8d1943c239647480ca16fe34bfc881b9276c9
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2023-07-23 05:27:36 +0700

    Add explore

--- app/(site)/components/landing/Categorylanding.tsx
+++ app/(site)/components/landing/Categorylanding.tsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Categorylanding(props: any) {
+    const router = useRouter();
     return (
-        <div className="flex flex-col items-center justify-center cursor-pointer">
+        <div className="flex flex-col items-center justify-center cursor-pointer" onClick={() => router.push(`/explore?category=${props.nama}`)}>
             <Image src ={props.foto} width={80} height={80} alt="Category Name"/>
             <p className="text-center">{props.nama}</p>
         </div>
